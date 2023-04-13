c96e0b6480040b5845407de867c2ea05ba143de9
Jonathan Math <jonathanamath@gmail.com>
2023-04-13 11:36:00 -0400

    add my ip address and public key as local variables in the data stack

--- _infrastructure/stacks/terraform/data/locals.tf
+++ _infrastructure/stacks/terraform/data/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    my_ip = jsondecode(data.http.my_ip.body)["ip"]
-    public_key = file(var.public_key_file)
+  my_ip      = jsondecode(data.http.my_ip.body)["ip"]
+  public_key = file(var.public_key_file)
 }
